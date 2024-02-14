8e1a63d69518862430f315327d24036bcd53ec1e
fkingsh <12200403@inha.edu>
2024-02-14 16:50:20 +0900

    fibo

--- day20.py
+++ day20.py
@@ -1,45 +1,14 @@
-# self 문제 3-2 계수가 0인 항과 첫 번째 항의 +부호 출력X
-
-def print_poly(f_x) -> str:
-    '''
-
-    :param f_x:
-    :return:
-    '''
-    term = len(f_x) - 1
-    poly_expression = "f(x) = "
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-
-        if coefficient >= 0:
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
-        term = term - 1
-
-    return poly_expression
 
 
-def calculation_poly(x_value, f_x) -> int:
-    '''
-
-    :param x_value:
-    :param f_x:
-    :return:
-    '''
-    return_value = 0
-    term = len(f_x) - 1
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        return_value += coefficient * pow(x_value, term)
-        term = term - 1
-
-    return return_value
-
-
-fx = [2, 3, 4, 0, -9]
-
-if __name__ == "__main__":
-    print(print_poly(fx))
-    print(calculation_poly(int(input("x 값 : ")), fx))
+def fibo_repetition(num: int) -> int:
+    """
+    fibonacci function by repetition
+    :param num: integer number
+    :return: integer number
+    """
+    a = 0
+    b = 1
+    for _ in range(num):
+        a = b
+        b = a + b
+    return a
